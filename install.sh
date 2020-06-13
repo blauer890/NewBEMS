abc42128b320e200458e409e6b40d5863eb9ed34
blauer890 <blauer@mail.bradley.edu>
2020-06-12 22:19:35 -0500

    Fixed finding site-packages directory

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 # Assign the project directory variables
 project_dir=$(dirname $(readlink -f "$0"))
-relwebserver_dir="/WebServer/"
-webserver_dir=$project_dir$relwebserver_dir
+tempwebserver_dir="/WebServer/"
+webserver_dir=$project_dir$tempwebserver_dir
 
 # Update Linux
 sudo apt-get update
@@ -24,17 +24,22 @@ python3 -m venv venv
 # Install the python dependencies
 pip3 install -r requirements.txt
 
+# Get the python version
+chmod +x utils.py
+pyversion=$(./utils.py version)
+
 # Create a pth_directories.pth file in the
 # site-packages directory and add
 # the directories to be added to the PYTHON_PATH
 # variable to the file
-if [ -d "venv/lib/python3.*/site-packages/" ];
+if [[ -d venv/lib/$pyversion/site-packages/ ]];
 then
-  cd venv/lib/python3.*/site-packages/
+  cd venv/lib/$pyversion/site-packages/
   touch pth_directories.pth
   echo $webserver_dir > pth_directories.pth
   echo $project_dir > pth_directories.pth
   cd $project_dir
+  echo "Succesfully added directories to the PYTHON_PATH"
 else
   echo "Unable to add directories to the PYTHON_PATH"
 fi
